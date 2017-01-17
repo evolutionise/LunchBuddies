cb4803d53ff130fe1a4bb0c3819c3ab5d2f44c72
Alix Klingenberg <alix.klingenberg@pushpay.com>
2017-01-17 20:53:45 +1300

    Things are still terrible but the problem with it not randomising the entire list and not handling the case where there's an even number of people are fixed

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LunchBuddies
@@ -14,10 +15,10 @@ namespace LunchBuddies
 			//Not worrying about matching team mates (at this stage)
 			//What if there is an uneven number of people?
 			var rawBuddiesInputString = args[0];
-			var buddies = rawBuddiesInputString.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+			var buddies = rawBuddiesInputString.Split(new string[] {"\n"}, StringSplitOptions.None);
+			buddies = Shuffle(buddies);
 			var buddies1 = buddies.Take(buddies.Length/2).ToArray();
 			var buddies2 = buddies.Skip(buddies.Length/2).ToArray();
-			buddies1 = Shuffle(buddies1);
 			var pairs = MatchPairs(buddies1, buddies2);
 			foreach (var pair in pairs)
 			{
@@ -27,11 +28,11 @@ namespace LunchBuddies
 
 		static string[] Shuffle(string[] list)
 		{
-			
+
 			//Fisher Yates Shuffle! 
 			//Takes each element and swaps it with one of the preceeding elements. 
 			var random = new Random();
-			for(var i = 0; i < list.Length; i++)
+			for (var i = 0; i < list.Length; i++)
 			{
 				int j = random.Next(i);
 				var tmp = list[j];
@@ -41,37 +42,34 @@ namespace LunchBuddies
 			return list;
 		}
 
-		static IEnumerable<Biumvirate> MatchPairs(string[] list1, string[] list2)
+		static IEnumerable<LunchGroup> MatchPairs(string[] list1, string[] list2)
 		{
-			var pairs = list1.Select((t, i) => new Biumvirate() {Person1 = t, Person2 = list2[i]}).ToArray();
-			//what if list2 is longer than list1 by more than 1? Then this is screwed, but the problem is upstream
-			if (list2.Length > list1.Length)
+			var pairs = new List<LunchGroup>();
+			for (var i = 0; i < list1.Length; i++)
 			{
-				pairs[pairs.Length] = new Triumvirate() {Person1 = pairs.Last().Person1, Person2 = pairs.Last().Person2, Person3 = list2.Last()};
+				var group = new LunchGroup() { People = new string[] { list1[i], list2[i] } };
+				pairs.Add(group);
 			}
+			//what if list2 is longer than list1 by more than 1? Then this is screwed, but the problem is upstream
+			if (list2.Length <= list1.Length) return pairs;
+
+			var lastIndex = pairs.Count - 1;
+			var lastPair = pairs[lastIndex].People;
+			var lastThree = new string[] {lastPair[0], lastPair[1], list2.Last()};
+			pairs[lastIndex].People = lastThree;
 			return pairs;
 		}
 	}
 
-	class Biumvirate
+	class LunchGroup
 	{
-		public string Person1 { get; set; }
-		public string Person2 { get; set; }
+		public string[] People { get; set; }
 
-		public virtual void Print()
+		public void Print()
 		{
-			Console.WriteLine($"{Person1} is having lunch with {Person2} this week!");
-		}
-	}
-
-	class Triumvirate : Biumvirate
-	{
-		
-		public string Person3 { get; set; }
-
-		public override void Print()
-		{
-			Console.WriteLine($"{Person1} is having lunch with {Person2} and {Person3} this week!");
+			Console.WriteLine(People.Length == 2
+				? $"{People[0]} is having lunch with {People[1]} this week!"
+				: $"{People[0]} is having lunch with {People[1]} and {People[2]} this week!");
 		}
 	}
 }
